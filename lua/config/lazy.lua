dabcb63036d30498d7293d3860f3603bed3d11f0
fajaralfa <fajaralfrzi@gmail.com>
2025-08-21 21:26:22 +0700

    add completion

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,6 +32,19 @@ require("lazy").setup({
               { "mason-org/mason.nvim", opts = {} },
               "neovim/nvim-lspconfig",
           },
+      },
+      {
+          'hrsh7th/nvim-cmp',
+          dependencies = {
+              'hrsh7th/cmp-buffer',
+              'hrsh7th/cmp-path',
+              'hrsh7th/cmp-cmdline',
+              'hrsh7th/cmp-nvim-lsp',
+          }
+      },
+      {
+          'hrsh7th/cmp-vsnip',
+          'hrsh7th/vim-vsnip',
       }
   },
   -- Configure any other settings here. See the documentation for more details.
