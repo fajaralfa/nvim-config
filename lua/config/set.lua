0d5422950ca9c100131df7b62b9901eaae161484
fajaralfa <fajaralfrzi@gmail.com>
2025-10-25 06:46:37 +0700

    auto restore cursor pos

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -10,3 +10,14 @@ vim.opt.scrolloff = 8
 vim.g.mapleader = ' '
 vim.o.signcolumn = "yes"
 vim.cmd.colorscheme('cyberdream-light')
+-- Automatically restore cursor position when reopening a file
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
